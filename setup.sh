219b19b0067729c7296b019f694966b6398f8180
DevNTNghia <nghiangusi2003@gmail.com>
2018-05-25 14:22:23 +0700

    Update 1

---
+++ setup.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+sudo apt-get install fcitx-unikey
+sudo apt-get update
+sudo apt-get install grub-customizer
+sudo apt-get upgrade
+cd ~/Desktop
+sudo wget https://raw.githubusercontent.com/nghia2k/github/master/clonemb
+sudo wget https://raw.githubusercontent.com/nghia2k/github/master/clonepc
+sudo chown devntnghia clone*
+sudo chmod +x clone* 
+echo "Please install Google Chrome, Team Viewer"
